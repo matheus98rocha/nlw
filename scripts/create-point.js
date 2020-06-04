e0c84afc7d68aa0d1a80874c1820c04d7782bdba
Matheus Gonçalves Rocha <matheus98rocha@gmail.com>
2020-06-03 23:32:58 -0300

    Segunda Aula

---
+++ scripts/create-point.js
@@ -0,0 +1,41 @@
+
+//Função para preencher o dropdown das Estados.
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name = uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(res => res.json())
+    .then(states => {
+        for(state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+populateUFs()
+
+
+//Função para preencher o dropdown das Cidades.
+function getCities(event){
+    const citySelect = document.querySelector("[name = city]")
+    const stateInput = document.querySelector("[name = city]")
+
+    const indexOfSelectedState = event.target.selectIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+    
+
+    const ufValue = event.target.value
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url).then(res => res.json())
+    .then(cities => {
+        for(city of cities){
+            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    })
+}
+
+//Evento que verifica quando um estado foi alterado para preencher o dropdown das cidades.
+document.querySelector("select[name = uf]").addEventListener("change", getCities)
